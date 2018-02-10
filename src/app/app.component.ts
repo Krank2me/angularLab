1d07a1c3c7aa0398b88b1ccbd7a1f67f3870668a
Panda <panda@Krank2Me.local>
2018-02-10 11:20:14 -0500

    primer release

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,63 @@
 import { Component } from '@angular/core';
 
+import { NgForm, FormGroup, FormBuilder } from '@angular/forms'
+
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'lab-angular';
+
+  public userForm : FormGroup;
+  userItems: any;
+  user;
+
+  constructor(public afDB: AngularFireDatabase, private fb : FormBuilder) {
+    this.userItems = afDB.list('users').valueChanges();
+  }
+
+  ngOnInit() {
+
+    this.userForm = this.fb.group({
+      name : [],
+      hobbie : [],
+      birthday : [],
+      email : [],
+      photo : []
+    });
+
+  }
+
+  createUser(form : any){
+    console.log(form);
+    const itemsRef = this.afDB.list('users');
+    itemsRef.push({
+      id: this.generateUUID(),
+      "name" : form.name,
+      "hobbie" : form.hobbie,
+      "birthday" : form.birthday,
+      "email" : form.email,
+      "photo" : form.photo
+    });
+
+  }
+
+  generateUUID() {
+    let d = new Date().getTime();
+    const uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+      const r = (d + Math.random() * 16) % 16 | 0;
+      d = Math.floor(d / 16);
+      return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+    return uuid;
+  };
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,36 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { RouterModule, Routes } from '@angular/router';
+
+import { environment } from '../environments/environment';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAy56uLLf3PRLJiHXIUB-UXiYCs5QOXuIA",
+  authDomain: "angular-lab-2791f.firebaseapp.com",
+  databaseURL: "https://angular-lab-2791f.firebaseio.com",
+  projectId: "angular-lab-2791f",
+  storageBucket: "angular-lab-2791f.appspot.com",
+  messagingSenderId: "875841288729"
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
